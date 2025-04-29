d24b0480b8c0c57e111b958b6fc8287e11c24c5c
Prime-Fum-Star <primefuntv881@gmail.com>
2025-04-29 19:02:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,12 +52,12 @@ async def show_random_emojis(message):
     return emoji_message
     
 # Define the owner's user ID
-OWNER_ID = 7841292070 # Replace with the actual owner's user ID
+OWNER_ID = 6676704302 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [7841292070,-1002624447857,-1002260543763]
+SUDO_USERS = [6676704302,-1002624447857,-1002260543763]
 
-AUTH_CHANNEL = [-1002624447857,-1002260543763]
+AUTH_CHANNEL = [-1002202621782]
 
 # Function to check if a user is authorized
 def is_authorized(user_id: int) -> bool:
